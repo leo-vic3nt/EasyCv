260c4f8a9026e2351f118a89703ea54b03ead329
leo.vi <leonardo.vic3nte@gmail.com>
2024-08-01 19:07:41 -0300

    add font and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="m-auto min-h-full max-w-screen-2xl bg-gray-200"></div>;
+  return (
+    <div className="m-auto min-h-full max-w-screen-2xl bg-gray-200">
+      <h1>Teste</h1>
+    </div>
+  );
 }
 
 export default App;
