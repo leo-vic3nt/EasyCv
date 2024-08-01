78c24f67355deecf0c86f20bc982a81c8acb596a
leo.vi <leonardo.vic3nte@gmail.com>
2024-08-01 19:20:13 -0300

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="m-auto min-h-full max-w-screen-2xl bg-gray-200">
+    <div className="m-auto flex min-h-full max-w-screen-2xl justify-center bg-gray-200 px-6 py-8">
       <h1>Teste</h1>
     </div>
   );
