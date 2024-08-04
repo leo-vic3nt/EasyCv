e971612419022826dace0f0ab36c4af083dbe4c7
leo.vi <leonardo.vic3nte@gmail.com>
2024-08-04 14:41:56 -0300

    add default value on input blur

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { defaultData } from "../lib/constants";
 
 function Container({ children }) {
   return (
@@ -10,7 +11,7 @@ function Container({ children }) {
   );
 }
 
-function FormInput({ name, label, value, onChange, type }) {
+function FormInput({ name, label, value, onChange, onBlur, type }) {
   return (
     <div className="mb-4">
       <label
@@ -25,6 +26,7 @@ function FormInput({ name, label, value, onChange, type }) {
         className="focus:shadow-outline w-full appearance-none rounded-xl border bg-gray-200 px-3 py-2 leading-tight focus:outline-none"
         value={value}
         onChange={onChange}
+        onBlur={onBlur}
       />
     </div>
   );
@@ -32,23 +34,47 @@ function FormInput({ name, label, value, onChange, type }) {
 
 function DataForm() {
   const [fullName, setFullName] = useState("John Doe");
-  const [phone, setPhone] = useState("+55 85 991232312");
+  const [phone, setPhone] = useState("+55 85 994567732");
   const [email, setEmail] = useState("john.doe@email.com");
   const [location, setLocation] = useState("São Paulo, Brazil");
+
+  const handleBlur = useCallback((e) => {
+    const { id, value } = e.target;
+    if (value === "") {
+      switch (id) {
+        case "fullName":
+          setFullName(defaultData.fullName);
+          break;
+        case "phoneNumber":
+          setPhone(defaultData.phoneNumber);
+          break;
+        case "email":
+          setEmail(defaultData.email);
+          break;
+        case "location":
+          setLocation(defaultData.location);
+          break;
+        default:
+          break;
+      }
+    }
+  }, []);
 
   return (
     <form>
       <FormInput
         name="fullName"
         label="full name"
+        onBlur={handleBlur}
         value={fullName}
         onChange={(e) => setFullName(e.target.value)}
         type="text"
       />
 
       <FormInput
-        name="phone"
+        name="phoneNumber"
         label="phone number"
+        onBlur={handleBlur}
         value={phone}
         onChange={(e) => setPhone(e.target.value)}
         type="tel"
@@ -57,6 +83,7 @@ function DataForm() {
       <FormInput
         name="email"
         label="email"
+        onBlur={handleBlur}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         type="email"
@@ -65,6 +92,7 @@ function DataForm() {
       <FormInput
         name="location"
         label="location"
+        onBlur={handleBlur}
         value={location}
         onChange={(e) => setLocation(e.target.value)}
         type="text"
@@ -76,8 +104,8 @@ function DataForm() {
 function PersonalDetails() {
   return (
     <Container>
-      <h3 className="flex gap-1 items-center text-2xl font-bold">
-        <UserCircleIcon className="size-7"/>
+      <h3 className="flex items-center gap-1 text-2xl font-bold">
+        <UserCircleIcon className="size-7" />
         Personal Details
       </h3>
       <DataForm />
---
+++ src/lib/constants.js
@@ -0,0 +1,8 @@
+const defaultData = {
+  fullName: "John Doe",
+  phoneNumber: "+55 85 994567732",
+  email: "john.doe@email.com",
+  location: "São Paulo, Brazil",
+};
+
+export { defaultData };
