6952eb7c47a2f483c5c7618e0f6513fc3cc8b2f5
leo.vi <leonardo.vic3nte@gmail.com>
2024-08-09 19:19:46 -0300

    add styling to add btn

--- src/components/EducationData.jsx
+++ src/components/EducationData.jsx
@@ -1,9 +1,8 @@
 import { SideCard } from "./SideCard";
 import { AcademicCapIcon } from "@heroicons/react/24/outline";
-
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 // @todo Implement each item component, form to add item component
-
 
 function AddForm({ cvData, setCvData }) {
 	return <div />;
@@ -12,7 +11,12 @@ function AddForm({ cvData, setCvData }) {
 function EducationData({ cvData, setCvData }) {
 	return (
 		<SideCard title={"Education"} icon={<AcademicCapIcon />}>
-			<button type="button">Add</button>
+			<button
+				type="button"
+				className="mx-auto flex w-fit items-center rounded-lg border-2 border-slate-600 px-2 py-1 hover:bg-gray-200"
+			>
+				Add <PlusIcon className="size-5" />
+			</button>
 		</SideCard>
 	);
 }
