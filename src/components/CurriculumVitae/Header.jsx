79a9ea460a9c9bf82706a5fbfb246df6ec7e6a2e
leonardo-v <leonardo.vic3nte@gmail.com>
2024-08-20 22:01:07 -0300

    finish cv header

--- src/components/CurriculumVitae/Header.jsx
+++ src/components/CurriculumVitae/Header.jsx
@@ -1,19 +1,47 @@
+import clsx from "clsx";
+
 function Header({ cvData, cvFont }) {
 	const { fullName, phoneNumber, location, email, github, linkedin } = cvData;
 
 	return (
 		<div className="flex flex-col items-center gap-3">
-			<h1 className="font-semibold text-4xl">{fullName}</h1>
+			<h1
+				className={clsx(
+					"font-semibold",
+					cvFont === "font-serif" ? "text-5xl" : "text-4xl",
+				)}
+			>
+				{fullName}
+			</h1>
 
-			<div className="flex gap-3">
+			{/* First row */}
+			<div
+				className={clsx("flex gap-3", cvFont === "font-serif" ? "text-lg" : "")}
+			>
 				<p>{phoneNumber}</p>
 				<p className="border-black border-l-2 pl-2">{email}</p>
 				<p className="border-black border-l-2 pl-2">{location}</p>
 			</div>
 
-			<div>
-				<p>{github}</p>
-				<p>{linkedin}</p>
+			{/* Second row */}
+			<div className="flex gap-5">
+				<div className="flex items-center gap-1">
+					<img
+						src="public/github-142-svgrepo-com.svg"
+						alt="github icon"
+						className="size-5"
+					/>
+					<p className="text-lg">{github}</p>
+				</div>
+				<div className="flex items-center gap-1">
+					<img
+						src="public/linkedin-svgrepo-com.svg"
+						alt="linkedin icon"
+						className="size-5"
+					/>
+
+					<p className="text-lg">{linkedin}</p>
+				</div>
 			</div>
 		</div>
 	);
